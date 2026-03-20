d886699e0d5ce8cf9f48efe90bb8f2c51934d7b1
matheus_bernardocode <matheus_ruekl@estudante.sesisenai.org.br>
2026-03-20 08:18:28 -0300

    Exercício 1 finalizado

--- exercício_01.js
+++ exercício_01.js
@@ -1,14 +1,13 @@
-let soma = 0;
-let continuar = true;
+function ex1() {
+  let soma = 0;
+  let continuar = true;
 
-while (continuar) {
-  let numero = Number(prompt("Digite um número:"));
-  soma += numero;
+  while (continuar) {
+    let numero = Number(prompt("Digite um número:"));
+    soma += numero;
 
-  let resposta = prompt("Deseja continuar? (s/n)");
-  if (resposta.toLowerCase() !== "s") {
-    continuar = false;
+    let resp = prompt("Continuar? (s/n)");
+    if (resp !== "s") continuar = false;
   }
-}
 
-console.log("Soma total:", soma);
+  console.log("Soma:", soma); }
