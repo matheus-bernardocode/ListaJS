5ff3a4c030abaadc174beb2beae49404fea8676e
matheus_ruekl <matheus_ruekl@estudante.sesisenai.org.br>
2026-05-13 14:47:16 -0300

    ADD- Finalizei o ex06.

---
+++ exercício_06.js
@@ -0,0 +1,15 @@
+function ex6() {
+  let letra = prompt("Digite uma letra:").toLowerCase();
+
+  if (
+    letra === "a" ||
+    letra === "e" ||
+    letra === "i" ||
+    letra === "o" ||
+    letra === "u"
+  ) {
+    console.log("É uma vogal.");
+  } else {
+    console.log("É uma consoante.");
+  }
+}
