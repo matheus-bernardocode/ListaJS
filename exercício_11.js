f3fe42227c9a9ecdb439dc16c033751fdaf01d54
matheus_ruekl <matheus_ruekl@estudante.sesisenai.org.br>
2026-05-13 14:54:46 -0300

    ADD- Finalizei o ultimo exercicio, 11.

---
+++ exercício_11.js
@@ -0,0 +1,21 @@
+function ex11() {
+  let salario = Number(prompt("Digite o salário:"));
+
+  let taxa = 0;
+
+  if (salario <= 1000) {
+    taxa = 0.08;
+  } else if (salario <= 1500) {
+    taxa = 0.085;
+  } else {
+    taxa = 0.09;
+  }
+
+  let desconto = salario * taxa;
+  let salarioLiquido = salario - desconto;
+
+  console.log("Salário informado:", salario);
+  console.log("Taxa aplicada:", (taxa * 100) + "%");
+  console.log("Valor do desconto:", desconto);
+  console.log("Salário líquido:", salarioLiquido);
+}
